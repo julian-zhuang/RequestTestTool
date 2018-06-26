2c792bf8157f285bbf764edf076da2443dacdfea
julian-zhuang <zhuangyuan0823@live.cn>
2018-06-26 14:42:10 +0800

    ui布局修改

--- RequestTestTool/GeneratedFiles/ui_RequestTestTool.h
+++ RequestTestTool/GeneratedFiles/ui_RequestTestTool.h
@@ -22,6 +22,7 @@
 #include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QSplitter>
 #include <QtWidgets/QTableWidget>
 #include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QVBoxLayout>
@@ -34,9 +35,13 @@ class Ui_RequestTestToolClass
 public:
     QWidget *centralWidget;
     QGridLayout *gridLayout;
+    QSplitter *splitter;
+    QWidget *layoutWidget;
     QVBoxLayout *verticalLayout;
     QTableWidget *tableWidget_Header;
     QTableWidget *tableWidget_values;
+    QWidget *layoutWidget1;
+    QVBoxLayout *verticalLayout_4;
     QVBoxLayout *verticalLayout_3;
     QHBoxLayout *horizontalLayout_3;
     QLabel *label_2;
@@ -72,31 +77,51 @@ public:
         gridLayout->setContentsMargins(11, 11, 11, 11);
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
         gridLayout->setContentsMargins(1, 1, 1, 1);
-        verticalLayout = new QVBoxLayout();
-        verticalLayout->setSpacing(0);
+        splitter = new QSplitter(centralWidget);
+        splitter->setObjectName(QStringLiteral("splitter"));
+        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(splitter->sizePolicy().hasHeightForWidth());
+        splitter->setSizePolicy(sizePolicy);
+        splitter->setOrientation(Qt::Horizontal);
+        splitter->setOpaqueResize(true);
+        splitter->setHandleWidth(5);
+        splitter->setChildrenCollapsible(true);
+        layoutWidget = new QWidget(splitter);
+        layoutWidget->setObjectName(QStringLiteral("layoutWidget"));
+        verticalLayout = new QVBoxLayout(layoutWidget);
+        verticalLayout->setSpacing(3);
+        verticalLayout->setContentsMargins(11, 11, 11, 11);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
-        tableWidget_Header = new QTableWidget(centralWidget);
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        tableWidget_Header = new QTableWidget(layoutWidget);
         tableWidget_Header->setObjectName(QStringLiteral("tableWidget_Header"));
         tableWidget_Header->setMinimumSize(QSize(300, 300));
 
         verticalLayout->addWidget(tableWidget_Header);
 
-        tableWidget_values = new QTableWidget(centralWidget);
+        tableWidget_values = new QTableWidget(layoutWidget);
         tableWidget_values->setObjectName(QStringLiteral("tableWidget_values"));
         tableWidget_values->setMinimumSize(QSize(300, 300));
 
         verticalLayout->addWidget(tableWidget_values);
 
-
-        gridLayout->addLayout(verticalLayout, 0, 0, 2, 1);
-
+        splitter->addWidget(layoutWidget);
+        layoutWidget1 = new QWidget(splitter);
+        layoutWidget1->setObjectName(QStringLiteral("layoutWidget1"));
+        verticalLayout_4 = new QVBoxLayout(layoutWidget1);
+        verticalLayout_4->setSpacing(2);
+        verticalLayout_4->setContentsMargins(11, 11, 11, 11);
+        verticalLayout_4->setObjectName(QStringLiteral("verticalLayout_4"));
+        verticalLayout_4->setContentsMargins(0, 0, 0, 0);
         verticalLayout_3 = new QVBoxLayout();
         verticalLayout_3->setSpacing(0);
         verticalLayout_3->setObjectName(QStringLiteral("verticalLayout_3"));
         horizontalLayout_3 = new QHBoxLayout();
         horizontalLayout_3->setSpacing(0);
         horizontalLayout_3->setObjectName(QStringLiteral("horizontalLayout_3"));
-        label_2 = new QLabel(centralWidget);
+        label_2 = new QLabel(layoutWidget1);
         label_2->setObjectName(QStringLiteral("label_2"));
         label_2->setMinimumSize(QSize(0, 25));
         label_2->setMaximumSize(QSize(16777215, 25));
@@ -105,24 +130,34 @@ public:
 
         horizontalLayout_3->addWidget(label_2);
 
-        comboBox_ShowType_3 = new QComboBox(centralWidget);
+        comboBox_ShowType_3 = new QComboBox(layoutWidget1);
         comboBox_ShowType_3->setObjectName(QStringLiteral("comboBox_ShowType_3"));
-        comboBox_ShowType_3->setMinimumSize(QSize(60, 30));
-        comboBox_ShowType_3->setMaximumSize(QSize(60, 30));
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(comboBox_ShowType_3->sizePolicy().hasHeightForWidth());
+        comboBox_ShowType_3->setSizePolicy(sizePolicy1);
+        comboBox_ShowType_3->setMinimumSize(QSize(60, 25));
+        comboBox_ShowType_3->setMaximumSize(QSize(60, 25));
 
         horizontalLayout_3->addWidget(comboBox_ShowType_3);
 
 
         verticalLayout_3->addLayout(horizontalLayout_3);
 
-        textBrowser_Request = new QTextBrowser(centralWidget);
+        textBrowser_Request = new QTextBrowser(layoutWidget1);
         textBrowser_Request->setObjectName(QStringLiteral("textBrowser_Request"));
+        QSizePolicy sizePolicy2(QSizePolicy::MinimumExpanding, QSizePolicy::Expanding);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(textBrowser_Request->sizePolicy().hasHeightForWidth());
+        textBrowser_Request->setSizePolicy(sizePolicy2);
         textBrowser_Request->setMinimumSize(QSize(300, 0));
 
         verticalLayout_3->addWidget(textBrowser_Request);
 
 
-        gridLayout->addLayout(verticalLayout_3, 0, 1, 1, 1);
+        verticalLayout_4->addLayout(verticalLayout_3);
 
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setSpacing(0);
@@ -130,7 +165,7 @@ public:
         horizontalLayout_2 = new QHBoxLayout();
         horizontalLayout_2->setSpacing(0);
         horizontalLayout_2->setObjectName(QStringLiteral("horizontalLayout_2"));
-        label = new QLabel(centralWidget);
+        label = new QLabel(layoutWidget1);
         label->setObjectName(QStringLiteral("label"));
         label->setMinimumSize(QSize(0, 25));
         label->setMaximumSize(QSize(16777215, 25));
@@ -139,17 +174,17 @@ public:
 
         horizontalLayout_2->addWidget(label);
 
-        comboBox_ShowType_2 = new QComboBox(centralWidget);
+        comboBox_ShowType_2 = new QComboBox(layoutWidget1);
         comboBox_ShowType_2->setObjectName(QStringLiteral("comboBox_ShowType_2"));
-        comboBox_ShowType_2->setMinimumSize(QSize(60, 30));
-        comboBox_ShowType_2->setMaximumSize(QSize(60, 30));
+        comboBox_ShowType_2->setMinimumSize(QSize(60, 25));
+        comboBox_ShowType_2->setMaximumSize(QSize(60, 25));
 
         horizontalLayout_2->addWidget(comboBox_ShowType_2);
 
 
         verticalLayout_2->addLayout(horizontalLayout_2);
 
-        textBrowser_Response = new QTextBrowser(centralWidget);
+        textBrowser_Response = new QTextBrowser(layoutWidget1);
         textBrowser_Response->setObjectName(QStringLiteral("textBrowser_Response"));
         textBrowser_Response->setEnabled(true);
         textBrowser_Response->setMinimumSize(QSize(300, 0));
@@ -157,7 +192,11 @@ public:
         verticalLayout_2->addWidget(textBrowser_Response);
 
 
-        gridLayout->addLayout(verticalLayout_2, 1, 1, 1, 1);
+        verticalLayout_4->addLayout(verticalLayout_2);
+
+        splitter->addWidget(layoutWidget1);
+
+        gridLayout->addWidget(splitter, 0, 0, 1, 1);
 
         horizontalLayout = new QHBoxLayout();
         horizontalLayout->setSpacing(0);
@@ -165,73 +204,75 @@ public:
         horizontalLayout->setSizeConstraint(QLayout::SetDefaultConstraint);
         comboBox_RequestType = new QComboBox(centralWidget);
         comboBox_RequestType->setObjectName(QStringLiteral("comboBox_RequestType"));
-        comboBox_RequestType->setMinimumSize(QSize(100, 30));
-        comboBox_RequestType->setMaximumSize(QSize(100, 30));
+        comboBox_RequestType->setMinimumSize(QSize(100, 25));
+        comboBox_RequestType->setMaximumSize(QSize(100, 25));
 
         horizontalLayout->addWidget(comboBox_RequestType);
 
         pushButton_Connect = new QPushButton(centralWidget);
         pushButton_Connect->setObjectName(QStringLiteral("pushButton_Connect"));
-        pushButton_Connect->setMinimumSize(QSize(60, 30));
-        pushButton_Connect->setMaximumSize(QSize(30, 30));
+        pushButton_Connect->setMinimumSize(QSize(60, 25));
+        pushButton_Connect->setMaximumSize(QSize(30, 25));
 
         horizontalLayout->addWidget(pushButton_Connect);
 
         lineEdit_Address = new QLineEdit(centralWidget);
         lineEdit_Address->setObjectName(QStringLiteral("lineEdit_Address"));
-        QSizePolicy sizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
-        sizePolicy.setHorizontalStretch(0);
-        sizePolicy.setVerticalStretch(0);
-        sizePolicy.setHeightForWidth(lineEdit_Address->sizePolicy().hasHeightForWidth());
-        lineEdit_Address->setSizePolicy(sizePolicy);
-        lineEdit_Address->setMinimumSize(QSize(557, 30));
-        lineEdit_Address->setMaximumSize(QSize(65535, 30));
+        QSizePolicy sizePolicy3(QSizePolicy::Expanding, QSizePolicy::Fixed);
+        sizePolicy3.setHorizontalStretch(0);
+        sizePolicy3.setVerticalStretch(0);
+        sizePolicy3.setHeightForWidth(lineEdit_Address->sizePolicy().hasHeightForWidth());
+        lineEdit_Address->setSizePolicy(sizePolicy3);
+        lineEdit_Address->setMinimumSize(QSize(557, 25));
+        lineEdit_Address->setMaximumSize(QSize(65535, 25));
 
         horizontalLayout->addWidget(lineEdit_Address);
 
         pushButton_Request = new QPushButton(centralWidget);
         pushButton_Request->setObjectName(QStringLiteral("pushButton_Request"));
-        pushButton_Request->setMinimumSize(QSize(60, 30));
-        pushButton_Request->setMaximumSize(QSize(60, 30));
+        pushButton_Request->setMinimumSize(QSize(60, 25));
+        pushButton_Request->setMaximumSize(QSize(60, 25));
 
         horizontalLayout->addWidget(pushButton_Request);
 
         label_SendDatalenth = new QLabel(centralWidget);
         label_SendDatalenth->setObjectName(QStringLiteral("label_SendDatalenth"));
-        label_SendDatalenth->setMinimumSize(QSize(0, 30));
-        label_SendDatalenth->setMaximumSize(QSize(16777215, 30));
+        label_SendDatalenth->setMinimumSize(QSize(0, 25));
+        label_SendDatalenth->setMaximumSize(QSize(16777215, 25));
 
         horizontalLayout->addWidget(label_SendDatalenth);
 
         lcdNumber_Send = new QLCDNumber(centralWidget);
         lcdNumber_Send->setObjectName(QStringLiteral("lcdNumber_Send"));
-        lcdNumber_Send->setMaximumSize(QSize(16777215, 30));
+        lcdNumber_Send->setMinimumSize(QSize(0, 25));
+        lcdNumber_Send->setMaximumSize(QSize(16777215, 25));
         lcdNumber_Send->setDigitCount(5);
 
         horizontalLayout->addWidget(lcdNumber_Send);
 
         label_RecvDatalenth = new QLabel(centralWidget);
         label_RecvDatalenth->setObjectName(QStringLiteral("label_RecvDatalenth"));
-        label_RecvDatalenth->setMinimumSize(QSize(0, 30));
-        label_RecvDatalenth->setMaximumSize(QSize(16777215, 30));
+        label_RecvDatalenth->setMinimumSize(QSize(0, 25));
+        label_RecvDatalenth->setMaximumSize(QSize(16777215, 25));
 
         horizontalLayout->addWidget(label_RecvDatalenth);
 
         lcdNumber_Recv = new QLCDNumber(centralWidget);
         lcdNumber_Recv->setObjectName(QStringLiteral("lcdNumber_Recv"));
-        lcdNumber_Recv->setMaximumSize(QSize(16777215, 30));
+        lcdNumber_Recv->setMinimumSize(QSize(0, 25));
+        lcdNumber_Recv->setMaximumSize(QSize(16777215, 25));
 
         horizontalLayout->addWidget(lcdNumber_Recv);
 
         comboBox_ShowType = new QComboBox(centralWidget);
         comboBox_ShowType->setObjectName(QStringLiteral("comboBox_ShowType"));
-        comboBox_ShowType->setMinimumSize(QSize(60, 30));
-        comboBox_ShowType->setMaximumSize(QSize(60, 30));
+        comboBox_ShowType->setMinimumSize(QSize(60, 25));
+        comboBox_ShowType->setMaximumSize(QSize(60, 25));
 
         horizontalLayout->addWidget(comboBox_ShowType);
 
 
-        gridLayout->addLayout(horizontalLayout, 2, 0, 1, 2);
+        gridLayout->addLayout(horizontalLayout, 1, 0, 1, 1);
 
         RequestTestToolClass->setCentralWidget(centralWidget);
 
